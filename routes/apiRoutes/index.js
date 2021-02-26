bec1551bc2350143774920cd0eb67a4dd6df9fb6
Matt Gross <mbgross111@gmail.com>
2021-02-26 15:28:13 -0700

    finished server set up and middleware for html and api endpoints

---
+++ routes/apiRoutes/index.js
@@ -0,0 +1,34 @@
+const router = require('express').Router();
+const notes = require('../../db/db.json');
+const { filterByQuery, findById, createNewNote, validateNote } = require('../../lib/notes');
+
+router.get('/notes', (req, res) => {
+  let results = notes
+  if (req.query) {
+    results = filterByQuery(req.query, results);
+  }
+  res.json(results);
+});
+
+router.get('/notes/:id', (req, res) => {
+  console.log(req.params);
+  const result = findById(req.params.id, notes);
+  if (result) {
+    res.json(result);
+  } else {
+    res.send(404)
+  }
+});
+
+router.post('/notes', (req, res) => {
+  // change id generator when we add delete notes capabilities
+  req.body.id = notes.length.toString()
+  if(!validateNote(req.body)) {
+    res.status(400).send('Improperly formatted note');
+  } else {
+    const note = createNewNote(req.body, notes);
+    res.json(note);
+  }
+})
+
+module.exports = router;
