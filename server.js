7ee2919feb3f31579b79ef0ebe387f3f37e3c454
Matt Gross <mbgross111@gmail.com>
2021-02-27 23:53:09 -0700

    version 1.0

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
 const htmlRoutes = require('./routes/htmlRoutes');
 const apiRoutes = require('./routes/apiRoutes');
-const express = require('express')
+const express = require('express');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-app.use(express.urlencoded({ extended:true }))
+app.use(express.urlencoded({ extended:true }));
 app.use(express.json());
 
 app.use('/', htmlRoutes);
 app.use('/api', apiRoutes);
 
-app.use(express.static('public'))
+app.use(express.static('public'));
 
 app.listen(PORT, () => {
   console.log(`Server on port ${PORT}`)
